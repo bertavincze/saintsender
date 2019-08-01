efd57fa60e4d9fd6f2c5f67e9a480031df1605fd
Berta Vincze <berta.vincze@gmail.com>
2019-08-01 23:33:53 +0200

    Add wrapper classes to store additional info

---
+++ SaintSender/SaintSender/FolderInfo.cs
@@ -0,0 +1,21 @@
+﻿using MailKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender
+{
+    public class FolderInfo
+    {
+        public FolderInfo(IMailFolder mailfolder, int size)
+        {
+            MailFolder = mailfolder;
+            FolderSize = size;
+        }
+
+        public IMailFolder MailFolder { get; }
+        public int FolderSize { get; set; }
+    }
+}
---
+++ SaintSender/SaintSender/MailInfo.cs
@@ -0,0 +1,24 @@
+﻿using MailKit;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender
+{
+    public class MailInfo
+    {
+        public MailInfo(MimeMessage message, UniqueId uid, IMailFolder parentFolder)
+        {
+            Message = message;
+            Uid = uid;
+            ParentFolder = parentFolder;
+        }
+
+        public MimeMessage Message { get; }
+        public UniqueId Uid { get; }
+        public IMailFolder ParentFolder { get; }
+    }
+}
