ec575adf4d1c3bef4085ac29db9c502988255d36
Berta Vincze <berta.vincze@gmail.com>
2019-08-01 23:36:42 +0200

    Add functions to load emails from a specific folder & send new emails

--- SaintSender/SaintSender/MailRepository.cs
+++ SaintSender/SaintSender/MailRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit;
 using MailKit.Net.Imap;
+using MailKit.Net.Smtp;
 using MailKit.Search;
 using MailKit.Security;
 using MimeKit;
@@ -24,35 +25,41 @@ namespace SaintSender
         }
 
         public int MaxEmails { get; set; }
+        public int NumOfFolders { get; private set; }
+        public DateTime DateFrom { get; set; }
         public UserData UserData { get; }
 
-        public void GetAllMails(IProgress<MimeMessage> progress, [Optional] DateTime dateFrom)
+        public void GetAllMails(IProgress<MailInfo> progress, [Optional] IMailFolder folder)
         {
-            if (dateFrom == new DateTime(0001, 01, 01, 0, 0, 0)) dateFrom = DateTime.Today.AddDays(-7);
-
             using (ImapClient client = new ImapClient())
             {
                 client.Connect(mailServer, port, ssl);
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
                 client.Authenticate(UserData.Email, UserData.Password);
 
-                IMailFolder inbox = client.Inbox;
-                inbox.Open(FolderAccess.ReadOnly);
-                SearchResults results = inbox.Search(SearchOptions.All, SearchQuery.DeliveredAfter(dateFrom));
+                if (folder == null) { folder = client.Inbox; }
+                else
+                {
+                    folder = client.GetFolder(folder.FullName);
+                }
+
+                folder.Open(FolderAccess.ReadOnly);
+                SearchResults results = folder.Search(SearchOptions.All, SearchQuery.DeliveredAfter(DateFrom));
 
                 if (results.UniqueIds.Count < MaxEmails) MaxEmails = results.UniqueIds.Count;
 
                 for (int i = MaxEmails - 1; i >= 0; i--)
                 {
-                    MimeMessage message = inbox.GetMessage(results.UniqueIds[i]);
+                    MailInfo message = new MailInfo(folder.GetMessage(results.UniqueIds[i]), results.UniqueIds[i], folder);
                     progress.Report(message);
                 }
 
+                folder.Close();
                 client.Disconnect(true);
             }
         }
 
-        public void GetAllFolders(IProgress<IMailFolder> progress)
+        public void GetAllFolders(IProgress<FolderInfo> progress)
         {
             using (ImapClient client = new ImapClient())
             {
@@ -60,19 +67,35 @@ namespace SaintSender
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
                 client.Authenticate(UserData.Email, UserData.Password);
 
-                var folders = client.GetFolders(new FolderNamespace('/', "[Gmail]"));
+                var folders = client.GetFolders(new FolderNamespace('/', ""));
+                NumOfFolders = folders.Count;
 
                 foreach (var folder in folders)
                 {
-                    folder.Open(FolderAccess.ReadOnly);
-                    progress.Report(folder);
-                }
+                    try
+                    {
+                        folder.Open(FolderAccess.ReadOnly);
+                        FolderInfo folderInfo = new FolderInfo(folder, 0);
+                        var emails = folder.Search(SearchQuery.All);
 
+                        foreach (var email in emails)
+                        {
+                            folderInfo.FolderSize++;
+                        }
+
+                        folder.Close();
+                        progress.Report(folderInfo);
+                    }
+                    catch (ImapCommandException)
+                    {
+                        continue;
+                    }
+                }
                 client.Disconnect(true);
             }
         }
 
-        public MimeMessage GetMailById(string messageId)
+        public MailInfo GetMailById(MailInfo mailInfo)
         {
             using (ImapClient client = new ImapClient())
             {
@@ -80,20 +103,78 @@ namespace SaintSender
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
                 client.Authenticate(UserData.Email, UserData.Password);
 
-                IMailFolder inbox = client.Inbox;
-                inbox.Open(FolderAccess.ReadOnly);
-                var results = inbox.Search(SearchQuery.HeaderContains("Message-ID", messageId));
+                IMailFolder folder = client.GetFolder(mailInfo.ParentFolder.FullName);
+                folder.Open(FolderAccess.ReadOnly);
+                var results = folder.Search(SearchQuery.Uids(new List<UniqueId> { mailInfo.Uid }));
 
-                foreach (var result in results)
+                MailInfo message = new MailInfo(folder.GetMessage(results[0]), results[0], folder);
+
+                client.Disconnect(true);
+                return message;
+            }
+        }
+
+        public FolderInfo GetFolderByName(string name)
+        {
+            using (ImapClient client = new ImapClient())
+            {
+                client.Connect(mailServer, port, ssl);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(UserData.Email, UserData.Password);
+
+                var folders = client.GetFolders(new FolderNamespace('/', ""));
+                foreach (var folder in folders)
                 {
-                    MimeMessage message = inbox.GetMessage(result);
-                    if (message.MessageId == messageId)
+                    if (folder.Name.Equals(name))
                     {
-                        return message;
+                        FolderInfo folderInfo = new FolderInfo(folder, folder.Count);
+                        return folderInfo;
                     }
                 }
                 client.Disconnect(true);
                 return null;
+            }
+        }
+
+        public void SendMail(string[] emailTo, string subject, string body, [Optional] string[] cc, [Optional] string[] bcc)
+        {
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.Connect(mailServer, port, ssl);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(UserData.Email, UserData.Password);
+
+                var message = new MimeMessage();
+
+                foreach (var address in emailTo)
+                {
+                    message.To.Add(new MailboxAddress(address));
+                }
+
+                if (cc != null)
+                {
+                    foreach (var copy in cc)
+                    {
+                        message.Cc.Add(new MailboxAddress(copy));
+                    }
+                }
+
+                if (bcc != null)
+                {
+                    foreach (var secretCopy in bcc)
+                    {
+                        message.Bcc.Add(new MailboxAddress(secretCopy));
+                    }
+                }
+                
+                message.From.Add(new MailboxAddress(UserData.Email));
+                message.Subject = subject;
+
+                message.Body = new TextPart("plain") { Text = body };
+
+                client.Send(message);
+
+                client.Disconnect(true);
             }
         }
 
